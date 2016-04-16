0c1ee15ce2946e8d7dfb7c5dfbaf1183e8ee7614
Łukasz Górny <lgorny@netizens.pl>
2016-04-16 22:17:45 +0200

    Add LogType icons and rooms/sessions logic

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,59 +1,67 @@
+var LogType = {
+    Error: "/img/error.png",
+    Assert: "/img/log.png",
+    Warning: "/img/warning.png",
+    Log: "/img/log.png",
+    Exception: "/img/error.png"
+};
+
 var UnityWebConsole = {
-  logs: [],
-  logsContainer: null,
+    logs: [],
+    logsContainer: null,
 
-  onDOMContentLoaded: function() {
-    UnityWebConsole.initSocket();
+    onDOMContentLoaded: function () {
+        UnityWebConsole.initSocket();
 
-    UnityWebConsole.logsContainer = document.getElementById("logs");
-    UnityWebConsole.logsContainer.scrollTop = UnityWebConsole.logsContainer.scrollHeight;
-  },
+        UnityWebConsole.logsContainer = document.getElementById("logs");
+        UnityWebConsole.logsContainer.scrollTop = UnityWebConsole.logsContainer.scrollHeight;
+    },
 
-  initSocket: function() {
-    UnityWebConsole.socket = io();
-    UnityWebConsole.socket.on("log", UnityWebConsole.onLog);
-  },
+    initSocket: function () {
+        UnityWebConsole.socket = io();
+        UnityWebConsole.socket.on("log", UnityWebConsole.onLog);
+    },
 
-  addLog: function(msg, stack, type) {
+    addLog: function (msg, stack, type) {
 
-    var scrollToBottom = false;
-    if(UnityWebConsole.logsContainer.scrollHeight - UnityWebConsole.logsContainer.scrollTop <= UnityWebConsole.logsContainer.clientHeight)
-      scrollToBottom = true;
+        var scrollToBottom = false;
+        if (UnityWebConsole.logsContainer.scrollHeight - UnityWebConsole.logsContainer.scrollTop <= UnityWebConsole.logsContainer.clientHeight)
+            scrollToBottom = true;
 
-    var log = document.createElement("button");
+        var log = document.createElement("button");
 
-    if(UnityWebConsole.logs.length % 2 == 1)
-      log.className = "log second"
-    else
-      log.className = "log"
+        if (UnityWebConsole.logs.length % 2 == 1)
+            log.className = "log second";
+        else
+            log.className = "log";
 
-    var imageContainer = document.createElement("p");
-    imageContainer.className = "log-icon";
-    var image = document.createElement("img");
-    image.src = "img/error.png";
-    imageContainer.appendChild(image);
+        var imageContainer = document.createElement("p");
+        imageContainer.className = "log-icon";
+        var image = document.createElement("img");
+        image.src = LogType[type];
+        imageContainer.appendChild(image);
 
-    var message = document.createElement("p");
-    message.innerHTML = msg + "<br>" + stack.split("\n")[0];
-    message.className = "log-message";
+        var message = document.createElement("p");
+        message.innerHTML = msg + "<br>" + stack.split("\n")[0];
+        message.className = "log-message";
 
-    log.appendChild(imageContainer);
-    log.appendChild(message);
-    log.addEventListener("click", function(e){e.currentTarget.className = "log select";});
-    document.getElementById('logs').appendChild(log);
+        log.appendChild(imageContainer);
+        log.appendChild(message);
+        log.addEventListener("click", function (e) {
+            e.currentTarget.className = "log select";
+        });
+        document.getElementById('logs').appendChild(log);
 
-    UnityWebConsole.logs.push(log);
+        UnityWebConsole.logs.push(log);
 
-    if(scrollToBottom)
-      UnityWebConsole.logsContainer.scrollTop = UnityWebConsole.logsContainer.scrollHeight;
-  },
+        if (scrollToBottom)
+            UnityWebConsole.logsContainer.scrollTop = UnityWebConsole.logsContainer.scrollHeight;
+    },
 
-  onLog: function(data) {
-    console.log(data);
-
-    var dataParsed = JSON.parse(data);
-    UnityWebConsole.addLog(dataParsed.m, dataParsed.s, dataParsed.t);
-  }
+    onLog: function (data) {
+        var dataParsed = JSON.parse(data);
+        UnityWebConsole.addLog(dataParsed.m, dataParsed.s, dataParsed.t);
+    }
 };
 
 document.addEventListener("DOMContentLoaded", UnityWebConsole.onDOMContentLoaded);
--- server.js
+++ server.js
@@ -5,23 +5,49 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
+const url = require('url');
 
 app.use(express.static('public_html'));
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 
 app.post('/log', onNewLog);
-
-io.on('connection', function(socket){
-  console.log('a user connected');
+app.get('/session/:id', function (req, res) {
+    console.log(req.params.id);
+    res.sendFile(__dirname + '/public_html/index.html');
 });
 
-function onNewLog(req, res)
-{
-  console.log(req.body);
-  io.emit("log", JSON.stringify(req.body));
-  res.sendStatus(200);
+io.on('connection', function (socket) {
+    var u = url.parse(socket.handshake.headers.referer);
+    var path = u.pathname.split("/");
+    path = cleanArray(path);
+
+    if (path.length == 2) {
+        if (path[0] == "session") {
+            socket.join(path[1].toLowerCase());
+            return;
+        }
+    }
+
+    socket.disconnect(true);
+});
+
+function onNewLog(req, res) {
+    io.to(req.body.id.toLowerCase()).emit("log", JSON.stringify(req.body));
+    res.sendStatus(200);
 }
 
-http.listen(config.SERVER_PORT, config.SERVER_IP, function(){
-  console.log( "Listening on " + config.SERVER_IP + ":" + config.SERVER_PORT );
+http.listen(config.SERVER_PORT, config.SERVER_IP, function () {
+    console.log("Listening on " + config.SERVER_IP + ":" + config.SERVER_PORT);
 });
+
+function cleanArray(actual) {
+    var newArray = new Array();
+    for (var i = 0; i < actual.length; i++) {
+        if (actual[i]) {
+            newArray.push(actual[i]);
+        }
+    }
+    return newArray;
+}
